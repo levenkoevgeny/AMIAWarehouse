ab0ab11ac29e71685cf76e84a66e56df95bfb5cc
Levenko Evgeny <levenko.evgeny@yandex.by>
2022-03-31 21:27:49 +0300

    New try to calculate )))

--- clothing/templatetags/card_tags.py
+++ clothing/templatetags/card_tags.py
@@ -58,9 +58,6 @@ def get_next_date_and_count(movement_list, args):
     # получаем список всех наименований в норме
     clothes_in_norm = Clothes.objects.filter(normitem__norm=norm)
 
-    # начало текущего полугодия
-    current_half_year_start_date = get_current_half_year_start_date()
-
     # проверяем в списке ли вещь
     if clothes in clothes_in_norm:
         # первый день текущего полугодия
@@ -80,50 +77,88 @@ def get_next_date_and_count(movement_list, args):
             result_count = norm_value_norm_count
         # если выдачи были
         else:
-            # получаем последнюю выдачу
-            last_issue = clothes_movement_list.order_by('-date_of_issue').first()
-            # print('last_issue', last_issue)
-            # рассчитываем полную поправку на увеличение срока (когда была замена позиции из аттестата, декрет и т.д.)
-            term_extension_sum = get_sum_term_extension(last_issue)
-            # print('term_extension_sum', term_extension_sum)
-            # рассчитываем дату от которой считаем начало выдачи
-            # (передаем последнюю выдачу и посчитанный оставшийся срок от сертификата)
-            start_calculating_date = get_start_calculating_date(last_issue,
-                                                                get_remaining_time_from_certificate(last_issue))
-            # print('start_calculating_date', start_calculating_date)
-            issued_count = 0
-            for desc in last_issue.descriptionitem_set.all():
-                issued_count += desc.count
+            # получаем последнюю базовую выдачу
+            last_base_issue = clothes_movement_list.filter(is_closed_loop=True).order_by('-date_of_issue').first()
+            if last_base_issue:
+                term_extension_sum = get_sum_term_extension(last_base_issue)
+                start_calculating_date = get_start_calculating_date(last_base_issue,
+                                                                    get_remaining_time_from_certificate(last_base_issue))
 
-            # print('issued_count', issued_count)
-            # print('months', wear_time_normalize(norm_value_wear_time_per_item * issued_count) + term_extension_sum)
-            result_date = start_calculating_date + relativedelta(
-                months=wear_time_normalize(norm_value_wear_time_per_item * issued_count) + term_extension_sum)
-            # print('result_date', result_date)
-
-            if result_date < current_half_year_start_date:
-                result_date = current_half_year_start_date
-                result_count = norm_value_norm_count
-            else:
-                if last_issue.is_closed_loop:
-                    result_count = norm_value_norm_count if (norm_value_norm_count - issued_count) <= 0 else (
-                            norm_value_norm_count - issued_count)
+                # если начальная дата отсчета + срок носки по норме + поправка меньша даты начала текущего полугодия, то
+                if start_calculating_date + relativedelta(
+                        months=norm_value_wear_time + term_extension_sum) < current_half_year_start_date:
+                    result_date = current_half_year_start_date
+                    result_count = norm_value_norm_count
                 else:
-                    last_closing_issue = clothes_movement_list.filter(is_closed_loop=True).order_by(
-                        '-date_of_issue').first()
-                    if last_closing_issue:
-                        # оставляем выдачи после последней
-                        clothes_movement_list_without_last_closing_issue = clothes_movement_list.filter(
-                            date_of_issue__gt=last_closing_issue.date_of_issue)
-                    else:
-                        clothes_movement_list_without_last_closing_issue = clothes_movement_list
-
+                    # получаем все выдачи от последней базовой до самой последней
+                    clothes_movement_list_rest = clothes_movement_list.filter(
+                        date_of_issue__gte=last_base_issue.date_of_issue)
+                    # просчитываем сумму всех выданных вещей
                     issued_count = 0
-                    for item in clothes_movement_list_without_last_closing_issue:
+                    for item in clothes_movement_list_rest:
                         for desc in item.descriptionitem_set.all():
                             issued_count += desc.count
-                    result_count = norm_value_norm_count if (norm_value_norm_count - issued_count) <= 0 else (
-                            norm_value_norm_count - issued_count)
+
+                    issued_count_normalized = norm_value_norm_count if (
+                            issued_count > norm_value_norm_count) else issued_count
+                    result_date = start_calculating_date + relativedelta(
+                        months=wear_time_normalize(
+                            norm_value_wear_time_per_item * issued_count_normalized) + term_extension_sum)
+                    if result_date < start_calculating_date:
+                        result_date = start_calculating_date
+                        result_count = norm_value_norm_count
+                    else:
+                        needed_count = norm_value_norm_count - issued_count_normalized
+                        result_count = norm_value_norm_count if (needed_count == 0) else needed_count
+
+            else:
+                pass
+
+        # else:
+        #     # получаем последнюю выдачу
+        #     last_issue = clothes_movement_list.order_by('-date_of_issue').first()
+        #     # print('last_issue', last_issue)
+        #     # рассчитываем полную поправку на увеличение срока (когда была замена позиции из аттестата, декрет и т.д.)
+        #     term_extension_sum = get_sum_term_extension(last_issue)
+        #     # print('term_extension_sum', term_extension_sum)
+        #     # рассчитываем дату от которой считаем начало выдачи
+        #     # (передаем последнюю выдачу и посчитанный оставшийся срок от сертификата)
+        #     start_calculating_date = get_start_calculating_date(last_issue,
+        #                                                         get_remaining_time_from_certificate(last_issue))
+        #     # print('start_calculating_date', start_calculating_date)
+        #     issued_count = 0
+        #     for desc in last_issue.descriptionitem_set.all():
+        #         issued_count += desc.count
+        #
+        #     # print('issued_count', issued_count)
+        #     # print('months', wear_time_normalize(norm_value_wear_time_per_item * issued_count) + term_extension_sum)
+        #     result_date = start_calculating_date + relativedelta(
+        #         months=wear_time_normalize(norm_value_wear_time_per_item * issued_count) + term_extension_sum)
+        #     # print('result_date', result_date)
+        #
+        #     if result_date < current_half_year_start_date:
+        #         result_date = current_half_year_start_date
+        #         result_count = norm_value_norm_count
+        #     else:
+        #         if last_issue.is_closed_loop:
+        #             result_count = norm_value_norm_count if (norm_value_norm_count - issued_count) <= 0 else (
+        #                     norm_value_norm_count - issued_count)
+        #         else:
+        #             last_closing_issue = clothes_movement_list.filter(is_closed_loop=True).order_by(
+        #                 '-date_of_issue').first()
+        #             if last_closing_issue:
+        #                 # оставляем выдачи после последней
+        #                 clothes_movement_list_without_last_closing_issue = clothes_movement_list.filter(
+        #                     date_of_issue__gt=last_closing_issue.date_of_issue)
+        #             else:
+        #                 clothes_movement_list_without_last_closing_issue = clothes_movement_list
+        #
+        #             issued_count = 0
+        #             for item in clothes_movement_list_without_last_closing_issue:
+        #                 for desc in item.descriptionitem_set.all():
+        #                     issued_count += desc.count
+        #             result_count = norm_value_norm_count if (norm_value_norm_count - issued_count) <= 0 else (
+        #                     norm_value_norm_count - issued_count)
 
     month = result_date.strftime("%m") if result_date else ""
     separator = "." if result_date else ""
